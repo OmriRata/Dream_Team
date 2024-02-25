d92940ccbe8bd4c573e6ba7a3c9b96d424b1da37
omrirata <omrata94@gmail.com>
2024-02-25 21:16:59 +0200

    add menu to the navbar to be responsive

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import {Link, NavLink} from 'react-router-dom'
 import "../style/Navbar.css"
 
 function Navbar(){
+    const [menuOpen,setMenuOpen] = useState(false);
+    const isOpen = () => setMenuOpen(!menuOpen);
     return (
         <nav>
             <Link className="title" to="/">Dream Team</Link>
-
-            <ul>
+            <div className="menu" onClick={isOpen}>
+                <span></span> 
+                <span></span>
+                <span></span>
+            </div>
+            <ul className={menuOpen?"open":""}>
                 <li>
                     <NavLink to="/login">Login</NavLink>
                 </li>
