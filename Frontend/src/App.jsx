1679fb85a8a0138a73bbfb0d02cf55e75841247a
omrirata <omrata94@gmail.com>
2024-02-28 21:55:12 +0200

    add use state to chanhe the background on login page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Navbar from './Components/Navbar';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes,useLocation } from 'react-router-dom';
 import Home from './Pages/Home'
 import Login from './Pages/Login'
 import Register from './Pages/Register'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoginPage, setBackground] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setBackground(location.pathname == "/login" ? true : false);
+  }, [location])
 
   return (
     <div className="App">
         <Navbar />
-        <div className='container'>
+        <div className={isLoginPage? "body-login container":"body-home container"}>
           <Routes>
               <Route path="/" element={< Home/> }/>
               <Route path="/login" element={< Login/> }/>
