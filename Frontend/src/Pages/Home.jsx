bf7bfe67acd548af299075f01bc3ac97a4359f87
omrirata <omrata94@gmail.com>
2024-04-03 18:41:45 +0300

    add button to navigate to league creation page

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import "../style/Home.css"
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
 
 
 function Home(){
+    const navigate = useNavigate()
+    const createLeague = ()=>{
+        navigate("/createLeague")
+    }
     return (
         <div>
             Dream Team
+            <br/>
+            <button onClick={createLeague} className="league"> create league</button>
+            <br/>
         </div>
     )
 }
