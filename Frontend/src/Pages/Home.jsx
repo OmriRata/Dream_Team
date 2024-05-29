68b458bdfba13c5b396b49e7c139749d6a8dbbfd
omrirata <omrata94@gmail.com>
2024-05-29 17:48:22 +0300

    edit the background home page

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -3,7 +3,9 @@ import "../style/Home.css"
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom"
 import Button from 'react-bootstrap/Button';
-
+import Home_bg from "../assets/home-banner-background.png"
+import Home_bg1 from "../assets/blue-white-gradient-color-.avif"
+import {FiArrowRight} from "react-icons/fi";
 
 function Home(){
     const navigate = useNavigate()
@@ -11,11 +13,25 @@ function Home(){
         navigate("/createLeague")
     }
     return (
-        <div>
-            Dream Team
-            <br/>
-            <Button variant="outline-primary" onClick={createLeague} className="league"> create league</Button>
-            <br/>
+        <div className="home-container">
+            <div className="home-banner-container">
+                <div className="home-bannerImage-container">
+                    <img src={Home_bg} alt="" />
+                </div>
+                <div className="home-text-section">
+                    <h1 className="primary-heading">
+                        Build Your Dream Team !
+                    </h1>
+                    <p className="primary-text">
+                        home-banner-background.png
+                        home-banner-background.png
+                    </p>
+                    <button className="secondary-button" onClick={createLeague}>
+                        Join Now <FiArrowRight />
+                    </button>
+                </div>
+            </div>
+            
         </div>
     )
 }
