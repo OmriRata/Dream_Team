69d94e7dd9038ae934457fe788cfe2dbc2ffefe9
omrirata <omrata94@gmail.com>
2024-05-30 17:18:55 +0300

    change and design the "League builder" page to create a new league for the user

--- Frontend/src/Pages/LeagueBuilder.jsx
+++ Frontend/src/Pages/LeagueBuilder.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React ,{useState,useEffect} from "react";
 import { Link } from "react-router-dom";
+import "../style/LeagueBuilder.css"
+import Ball from "../assets/soccer-ball-white-line-stadium.jpg"
+import FormInput from '../Components/FormInput'
+import { Flex, Text, Button } from '@radix-ui/themes';
+
+
 
 
 function LeagueBuilder(){
+    const fatchData = ()=>{
+        fetch('/api/teamId/barcelona')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+    useEffect(()=>{
+    },[])
 
     return (
-        <div>
-            craete league
+        <div className="league-builder">
+            <Flex direction="row" gap="2">
+                <Flex direction="column">
+                    <h1>Create Your Fantasy Football League</h1>
+                </Flex>
+            <form className="league-form">
+                <FormInput placeholder="League Name"/>
+                <FormInput placeholder="participants"/>
+                <FormInput placeholder="choose league"/>
+                <FormInput placeholder="starting budget"/>
+                <Button className="league-btn" color="crimson" variant="soft">Submit</Button>
+            </form>
+            </Flex>
         </div>
     )
 }
