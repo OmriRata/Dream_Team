fe904e3de8ae70cac4a7b993069172148d6bdba2
omrirata <omrata94@gmail.com>
2024-05-30 20:16:48 +0300

    add logo to the league builder page and design it

--- Frontend/src/Pages/LeagueBuilder.jsx
+++ Frontend/src/Pages/LeagueBuilder.jsx
@@ -1,7 +1,7 @@
 import React ,{useState,useEffect} from "react";
 import { Link } from "react-router-dom";
 import "../style/LeagueBuilder.css"
-import Ball from "../assets/soccer-ball-white-line-stadium.jpg"
+import logo from "../assets/download.png"
 import FormInput from '../Components/FormInput'
 import { Flex, Text, Button } from '@radix-ui/themes';
 
@@ -20,8 +20,9 @@ function LeagueBuilder(){
     return (
         <div className="league-builder">
             <Flex direction="row" gap="2">
-                <Flex direction="column">
+                <Flex className="logo-container" direction="column">
                     <h1>Create Your Fantasy Football League</h1>
+                    <img className="logo" src={logo} alt="" />
                 </Flex>
             <form className="league-form">
                 <FormInput placeholder="League Name"/>
