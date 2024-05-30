52cdd420bb14006ddff8b0f76dc5940e282c21fe
omrirata <omrata94@gmail.com>
2024-05-30 17:16:50 +0300

    add themes componenet fo the radix-ui libraray

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Theme } from '@radix-ui/themes';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Theme>
     <App />
+    </Theme>
     </BrowserRouter>
   </React.StrictMode>,
 )
