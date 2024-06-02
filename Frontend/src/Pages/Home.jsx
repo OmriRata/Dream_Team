3856c5196f5e0374dc5a9f17b7c1768dbaa0849e
omrirata <omrata94@gmail.com>
2024-06-02 14:40:45 +0300

    add player image for the home page

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -7,7 +7,10 @@ import Orange_bg from "../assets/home-banner-background.png"
 import Green_bg from "../assets/home-banner-background_green.png"
 import Blue_bg from "../assets/home-banner-background_blue.png"
 import Purple_bg from "../assets/home-banner-background_purple.png"
+import player from '../assets/soccer-player-team-sport-football-player-jersey-lucas.png'
+import player2 from '../assets/istockphoto-939168954-612x612-removebg-preview.png'
 import {FiArrowRight} from "react-icons/fi";
+import { Flex } from '@radix-ui/themes';
 
 function Home(props){
     const navigate = useNavigate()
@@ -27,18 +30,25 @@ function Home(props){
             </div>
             
         </div>
+        <Flex direction="row" gap="2">
+
         <div className="home-text-section">
-                    <h1 className="primary-heading">
-                        Build Your Dream Team !
-                    </h1>
-                    <p className="primary-text">
-                        home-banner-background.png
-                        home-banner-background.png
-                    </p>
-                    <button className="secondary-button" onClick={createLeague}>
-                        Join Now <FiArrowRight />
-                    </button>
+                        <h1 className="primary-heading">
+                            Build Your Dream Team !
+                        </h1>
+                        <p className="primary-text">
+                            home-banner-background.png
+                            home-banner-background.png
+                        </p>
+                        <button className="secondary-button" onClick={createLeague}>
+                            Join Now <FiArrowRight />
+                        </button>
                 </div>
+                <>
+                <img src={player} alt="" />
+                </>
+                </Flex>
+
         </>
     )
 }
