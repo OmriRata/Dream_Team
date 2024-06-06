57145d62e3c7f7766c350792893a0bb9ddddde9e
omrirata <omrata94@gmail.com>
2024-06-06 19:07:10 +0300

    fix the table for the search bar

--- Frontend/src/Components/PlayerSelection.jsx
+++ Frontend/src/Components/PlayerSelection.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import { TextField,Container,Box,Card,Flex,Avatar,Text, Button, Separator } from '@radix-ui/themes'
+import { TextField,Container,Box,Card,Flex,Avatar,Text, Button, Separator,ScrollArea } from '@radix-ui/themes'
 import '../style/PlayerSelection.css'
-
+import Data from '../Data/data'
+import { useEffect, useState } from 'react'
 
 
 function PlayerSelection() {
+    const [data,setData] = useState([]);
+    function abc(){
+        setData(Data)
+        console.log("first")
+    }
+    useEffect(() => {
+        abc()
+    }, [data])
+
     return (
         <div className='player-selection'>
             <div >
@@ -16,34 +26,39 @@ function PlayerSelection() {
                     <Button className='searchBtn'>Search</Button>
                     </Flex>
                 </form>
-                <table striped bordered hover>
+                
+                <table className='tbl' striped bordered hover>
+                <ScrollArea className='scroll' type="always" scrollbars="vertical" size="3" style={{ height: 700 ,color:'ActiveBorder'}}>
                 <tbody>
-                        <tr key={'1'}>
-                            <Box width='350px'>
-                                <Card size="1">
-                                    <Flex position="relative" gap="4" align="center">
-                                        <Avatar size="3" radius="full" fallback="T" color="indigo" />
-                                        <Box>
-                                        <Text as="div" size="2" weight="bold">
-                                            Teodros Girmay
-                                        </Text>
-                                        
-                                        <Text as="div" size="2" color="gray">
-                                            Engineering
-                                        </Text>
-                                        
-                                        </Box>
-                                    
-                                        <Button className='addBtn'>Add</Button>
-                                        
-                                        
-                                        
-                                    </Flex>
-                                </Card>
-                            </Box>
+                        {data.map((item,index)=>{
+                            return<tr key={index}>
+                                    <Box className='player-box' width='350px'>
+                                        <Card size="1">
+                                            <Flex position="relative" gap="4" align="center">
+                                                <Avatar size="3" src={item.photo} radius="full" fallback="T" color="indigo" />
+                                                <Box>
+                                                <Text as="div" size="2" weight="bold">
+                                                    {/* {data[0] && data[0].name} */}
+                                                    {item.name}
+                                                </Text>
+                                                
+                                                <Text as="div" size="2" color="gray">
+                                                    {/* Engineering */}
+                                                    {item.position}
+                                                </Text>
 
-                        </tr>
+                                                </Box>
+                                                <Button className='addBtn'>Add</Button>
+                                            </Flex>
+
+                                        </Card>
+                                    </Box>
+
+                                </tr>
+                        })}
                 </tbody>
+                </ScrollArea>
+
                 </table>
             </Container>
     </div>
