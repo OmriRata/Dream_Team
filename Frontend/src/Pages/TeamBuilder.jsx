0e6b792c2c81d8ebd2dc04a5eff5db5b0ed4bd42
omrirata <omrata94@gmail.com>
2024-06-06 13:42:28 +0300

    create new page for the creating team

--- Frontend/src/Pages/TeamBuilder.jsx
+++ Frontend/src/Pages/TeamBuilder.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Flex,Separator } from '@radix-ui/themes';
+import "../style/TeamBuilder.css"
+import soccer_field from '../assets/soccer_field2.jpg'
+import PlayerSelection from '../Components/PlayerSelection';
+import LineUp from '../Components/LineUp';
 
 function TeamBuilder() {
 return (
-    <div>TeamBuilder</div>
+        <Flex className="team-builder" direction="column" gap="2">
+            <div className="playerSelect" >
+                <PlayerSelection/>
+            </div>
+            <div className='Line-up'>
+                <LineUp/>
+            </div>
+        </Flex>
 )
 }
 
