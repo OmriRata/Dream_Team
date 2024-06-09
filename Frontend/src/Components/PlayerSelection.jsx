3299104111dac54143e14feb3d2c0f8142112f6a
omrirata <omrata94@gmail.com>
2024-06-09 23:55:05 +0300

    add filtring logic for the players search bar  + rmove the table that unused

--- Frontend/src/Components/PlayerSelection.jsx
+++ Frontend/src/Components/PlayerSelection.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
-import { TextField,Container,Box,Card,Flex,Avatar,Text, Button, Separator,ScrollArea } from '@radix-ui/themes'
+import {TextField,Container,Box,Card,Flex,Avatar,Text, Button, Separator,ScrollArea } from '@radix-ui/themes'
 import '../style/PlayerSelection.css'
 import Data from '../Data/data'
 import { useEffect, useState } from 'react'
 
 
-function PlayerSelection() {
+function PlayerSelection(props) {
     const [data,setData] = useState([]);
+    const [search,setSearch] = useState('');
+
     function abc(){
         setData(Data)
         console.log("first")
     }
+
+    const addPlayers = (i)=>{
+        props.setPlayers([
+            ...props.players,
+            i.photo
+        ]);
+
+    } 
     useEffect(() => {
         abc()
     }, [data])
@@ -22,18 +32,16 @@ function PlayerSelection() {
                 <h1 className='text-center mt-4'>Add Players</h1>
                 <form className='search-form'>
                     <Flex>
-                    <TextField.Root className='my-3' variant="soft" placeholder="Search Players…" />
+                    <TextField.Root className='my-3' variant="soft" onChange={(e)=>{setSearch(e.target.value)}} placeholder="Search Players…" />
                     <Button className='searchBtn'>Search</Button>
                     </Flex>
                 </form>
-                
-                <table className='tbl' striped bordered hover>
                 <ScrollArea className='scroll' type="always" scrollbars="vertical" size="3" style={{ height: 700 ,color:'ActiveBorder'}}>
-                <tbody>
-                        {data.map((item,index)=>{
-                            return<tr key={index}>
-                                    <Box className='player-box' width='350px'>
-                                        <Card size="1">
+                        {data.filter(i=>{
+                            return search.toLowerCase()===''? i: i.name.toLowerCase().includes(search);
+                        }).map((item,index)=>{
+                            return  <Box key={index} className='player-box' width='350px'>
+                                        <Card size="2">
                                             <Flex position="relative" gap="4" align="center">
                                                 <Avatar size="3" src={item.photo} radius="full" fallback="T" color="indigo" />
                                                 <Box>
@@ -47,19 +55,21 @@ function PlayerSelection() {
                                                     {item.position}
                                                 </Text>
 
-                                                </Box>
-                                                <Button className='addBtn'>Add</Button>
+                                                </Box> 
+                                                {item.id.toString().startsWith('2')?
+                                                <label className='price'>price : 31222M</label>
+                                                :    
+                                                <label className='price'>price : 22M</label>
+                                            } 
+                                                <Button color="cyan" variant="soft" onClick={()=>addPlayers(item)} className='addBtn'>Add</Button>
                                             </Flex>
 
                                         </Card>
                                     </Box>
 
-                                </tr>
                         })}
-                </tbody>
                 </ScrollArea>
 
-                </table>
             </Container>
     </div>
         </div>
