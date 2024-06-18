60cc292983b7a68b5b80c2a6025eb527a352e1ae
omrirata <omrata94@gmail.com>
2024-06-18 18:37:23 +0300

    create options to add and remove players to the lineUp Component accordig to their positions

--- Frontend/src/Components/LineUp.jsx
+++ Frontend/src/Components/LineUp.jsx
@@ -1,20 +1,65 @@
 import React, { useEffect } from 'react'
 import '../style/LineUp.css'
 import field from '../assets/soccer_field2.jpg'
-import {Avatar, Button} from '@radix-ui/themes'
-function LineUp({players}) {
-    
+import { Avatar, Button, Flex, Box, Grid,Card } from '@radix-ui/themes'
+import data from '../Data/data'
+function LineUp(props) {
+
+    const removePlayer = (player)=>{
+        const updatedPlayers = props.players.filter(p => p.id !== player.id);
+        props.setPlayers(updatedPlayers)
+    }
+
+    function addplayers(id, position) {
+        const player = props.players.find(p => p.id === id);
+        if (player && position == player.position) {
+            return <Box className='plyr' size="9" width='350px'>
+
+                    <Avatar className='plyrImg' size="5" src={player.photo} radius="full" fallback="T" color="indigo" />
+                    <Button size='1' color="red" variant="surface" onClick={()=>{removePlayer(player)}}>X</Button>
+
+                    <h5 className='plyrName'><span>{player.name}</span></h5>
+            </Box>
+        }
+    }
+    useEffect(()=>{
+    },[props.players])
     return (
         <div className='line-up'>
             <h1 className='teamH1'>My Team:</h1>
-            <div className="line">
-                {players.map((img,i)=>{
-                    return  <Avatar key={i} size="6" src={img} radius="full" fallback="T" color="indigo" />
-                })
-                }
-            </div>
+            <Flex className='line' direction="column" rows="4" gap="3" width="auto">
+                <div id='goalkeeper' className='position goalkeeper'>
+                    {
+                        props.players.map((player, i) => {
+                            return addplayers(player.id, "Goalkeeper")
+                        })
+                    }
+                </div>
+
+                <div id='defender' className='position defender'>
+                    {
+                        props.players.map((player, i) => {
+                            return addplayers(player.id, "Defender")
+                        })
+                    }
+                </div>
+                <div id='midfielder' className='position midfielder'>
+                    {
+                        props.players.map((player, i) => {
+                            return addplayers(player.id, "Midfielder")
+                        })
+                    }
+                </div>
+                <div id='attacker' className='position attacker'>
+                    {
+                        props.players.map((player, i) => {
+                            return addplayers(player.id, "Attacker")
+                        })
+                    }
+                </div>
+            </Flex>
             <Button className='applyBtn'> Apply </Button>
-            
+
         </div>
     )
 }
