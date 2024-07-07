55e02594ccad192beaf7a27afd3e776e14edf595
omrirata <omrata94@gmail.com>
2024-07-07 18:19:50 +0300

    add endpoints

--- Beckend/api/api.py
+++ Beckend/api/api.py
@@ -46,7 +46,16 @@ def getPlayersByTeam(team_id):
     print(response.json()["response"][0]["players"])
     return response.json()["response"][0]["players"]
 
+@api.route("/playersByLeague/<league_id>") 
+def getPlayersByLeague(league_id):
+    # querystring = {"team":team_id}
+    querystring = {"league":"39","season":"2020"}
 
+    
+    response = requests.get(URL+"players", headers=HEADERS, params=querystring)
+    return response.json()["response"]
+    print(response.json()["response"][0]["players"])
+    return response.json()["response"][0]["players"]
 
 @api.route("/teamId/<team_name>") 
 # @jwt_required()
