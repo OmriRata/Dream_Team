610ec38a9baa936a32f679812ad7a57c4fbc7e52
omrirata <omrata94@gmail.com>
2024-07-15 18:53:01 +0300

    add useLocation to gets the params for this page

--- Frontend/src/Pages/TeamBuilder.jsx
+++ Frontend/src/Pages/TeamBuilder.jsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import { Flex,Separator } from '@radix-ui/themes';
 import "../style/TeamBuilder.css"
 import soccer_field from '../assets/soccer_field2.jpg'
 import PlayerSelection from '../Components/PlayerSelection';
 import LineUp from '../Components/LineUp';
 
-function TeamBuilder() {
-    const [players,setPlayers] = useState([])
-    const [amount,setAmount] = useState('100M')
+function TeamBuilder(props) {
+    const location = useLocation();
+    const isEditMode = location.state?.isEditMode;
+    console.log(isEditMode)
 
+    const [players,setPlayers] = useState(location.state?.players?location.state?.players:[])
+    const [amount,setAmount] = useState('100M')
+    
 
 return (
         <Flex className="team-builder" direction="column" gap="2">
@@ -16,7 +21,7 @@ return (
                 <PlayerSelection setAmount={setAmount} setPlayers={setPlayers} players={players} />
             </div>
             <div className='Line-up'>
-                <LineUp amount={amount} isCreate={true} players={players} setPlayers={setPlayers}/>
+                <LineUp isEditMode={isEditMode?true:false} amount={amount} isCreate={true} players={players} setPlayers={setPlayers}/>
             </div>
         </Flex>
 )
