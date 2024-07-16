44e7f6e09b415a2c4540744c3fb3223ac14babb5
omrirata <omrata94@gmail.com>
2024-07-16 16:54:09 +0300

    change the table rows

--- Frontend/src/Components/Table.jsx
+++ Frontend/src/Components/Table.jsx
@@ -28,41 +28,60 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     },
     }));
 
-    function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-    }
+
+    // const rows = [
+    // createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    // createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    // createData('Eclair', 262, 16.0, 24, 6.0),
+    // createData('Cupcake', 305, 3.7, 67, 4.3),
+    // createData('Gingerbread', 356, 16.0, 49, 3.9),
+    // ];
+
+
+
+    export default function CustomizedTables(props) {
 
     const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+        createData('Eclair', 262, 16.0, 24, 6.0),
+        createData('Cupcake', 305, 3.7, 67, 4.3),
+        createData('Gingerbread', 356, 16.0, 49, 3.9),
     ];
+    function createData(leagueName, league_code, points, username,x) {
+        return { username, leagueName, league_code, points ,x};
+    }
+    // const rows = props.teams.map((team)=>{
 
-    export default function CustomizedTables() {
+    // })
+
     return (
     <TableContainer component={Paper}>
         <Table sx={{ minWidth: 700 }} aria-label="customized table">
         <TableHead>
             <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+            {/* <StyledTableCell>Dessert (100g serving)</StyledTableCell> */}
+            <StyledTableCell align="left">Calories</StyledTableCell>
+            <StyledTableCell align="left">Fat&nbsp;(g)</StyledTableCell>
+            <StyledTableCell align="left">Carbs&nbsp;(g)</StyledTableCell>
+            <StyledTableCell align='center'>Dessert (100g serving)</StyledTableCell>
+
+            {/* <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell> */}
             </TableRow>
         </TableHead>
         <TableBody>
             {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-                <StyledTableCell component="th" scope="row">
-                {row.name}
+            <StyledTableRow key={row.username}>
+                {/* <StyledTableCell component="th" scope="row">
+                {row.username}
+                </StyledTableCell> */}
+                <StyledTableCell align="left">{row.leagueName}</StyledTableCell>
+                <StyledTableCell align="left">{row.league_code}</StyledTableCell>
+                <StyledTableCell align="left">{row.points}</StyledTableCell>
+                <StyledTableCell align='center' component="th" scope="row">
+                {row.username}
                 </StyledTableCell>
-                <StyledTableCell align="right">{row.calories}</StyledTableCell>
-                <StyledTableCell align="right">{row.fat}</StyledTableCell>
-                <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-                <StyledTableCell align="right">{row.protein}</StyledTableCell>
+                {/* <StyledTableCell align="right">{row.protein}</StyledTableCell> */}
             </StyledTableRow>
             ))}
         </TableBody>
