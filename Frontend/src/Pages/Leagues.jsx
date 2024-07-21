6a19d1a1ad91bc1b85706d07fc2cfe600d920c00
omrirata <omrata94@gmail.com>
2024-07-21 17:25:14 +0300

    make an api call to get the leagues of the user

--- Frontend/src/Pages/Leagues.jsx
+++ Frontend/src/Pages/Leagues.jsx
@@ -2,15 +2,29 @@ import React,{ useEffect, useState }  from "react";
 import CustomizedTables from '../Components/Table'
 import "../style/Leagues.css"
 import {Flex, Button } from "@radix-ui/themes";
+import axios from "axios";
+
 
 
 
 
 function Leagues(){
     const getLeagues = async()=>{
-
+        try {
+            const response = await axios.post("/users/getUserLeagues", {
+                username:localStorage.getItem("username")
+            });
+            const data = response.data
+            console.log(JSON.parse(data.leagues))
+            console.log(data.username)
+        }
+        catch(error){
+            console.log(error)
+        }
     }
+
     useEffect(()=>{
+        console.log("Leagues Page")
         getLeagues()
     })
     return <div className="leagues">
